91d4163a3b14e435f7c7d969863258bca3894dae
jiyuw <jiyuw@uw.edu>
2021-02-27 14:54:55 -0800

    add retinals

---
+++ code/main_retinal.py
@@ -0,0 +1,117 @@
+from __future__ import print_function
+from __future__ import division
+import argparse
+import numpy as np
+from models_retinal import CHyVAE
+import os
+import torch
+import pandas as pd
+import cv2
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+
+class DC_dataset(Dataset):
+    def __init__(self, df, root_dir, type, target=None, transform=None):
+        self.df = df
+        self.target = target
+        self.root_dir = root_dir
+        self.transform = transform
+        self.type = type
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        pt_dir = self.df.loc[idx, 'ptid']
+        ins_dir = self.df.loc[idx, 'instance']
+        if self.target:
+            label = self.df.loc[idx, self.target]
+            label = torch.tensor(label)
+        else:
+            label = torch.tensor(np.nan)
+
+        if self.type == 'IR':
+            img_name = 'ir.png'
+        elif self.type == 'OCT':
+            middle = self.df.loc[idx, 'middle']
+            img_name = 'oct-0' + str(middle) + '.png'
+        img_path = os.path.join(self.root_dir, pt_dir, 'macOCT', ins_dir, img_name)
+        img_pre = cv2.imread(img_path, flags=0)
+        img = np.expand_dims(img_pre, axis=0)
+
+        # default transform
+        img = (img - np.float32(127.5)) / np.float32(127.5)
+        img = torch.tensor(img)
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
+
+
+def dataloader_create(train, test, root_dir, type='OCT', batch_size=100, shuffle=False, label = None, augmentation=None):
+    """
+    create train and test dataloaders directly from train and test csv
+
+    :param train: csv file for training set
+    :param test: csv file for testing set
+    :param root_dir: root directory for images
+    :param type: type of images
+    :param batch_size: batch_size
+    :param shuffle: shuffle indicator for dataloader
+    :param label: column used for label
+    :param augmentation: additional augmentation
+    :return:
+    """
+    train_df = pd.read_csv(train)
+    test_df = pd.read_csv(test)
+
+    train_set = DC_dataset(train_df, root_dir, type, label, augmentation)
+    test_set = DC_dataset(test_df, root_dir, type, label, augmentation)
+
+    train_loader = DataLoader(
+        train_set,
+        batch_size=batch_size,
+        shuffle=shuffle
+    )
+
+    test_loader = DataLoader(
+        test_set,
+        batch_size=batch_size,
+        shuffle=shuffle
+    )
+
+    return train_loader, test_loader
+
+
+def set_args(parser):
+    parser.add_argument('--dataset', required=True, help='2dshapes | correlated_ellipses')
+    parser.add_argument('--batch_size', type=int,
+                        default=50, help='input batch size')
+    parser.add_argument('--z_dim', type=int, default=20,
+                        help='latent vector dim')
+    parser.add_argument('--nu', type=int, default=33,
+                        help='degrees of freedom (> z_dim + 1)')
+    parser.add_argument('--n_epoch', type=int, default=100,
+                        help='numbers of training steps')
+    parser.add_argument('--run', type=int, default=None,
+                        help='run number')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser = set_args(parser)
+    args = parser.parse_args()
+
+    train_path = '/data/project/jiyu/dataset_prep/imgs_label_0213_train.csv'
+    test_path = '/data/project/jiyu/dataset_prep/imgs_label_0213_test.csv'
+    root = '/data/macoct/'
+    train, test = dataloader_create(train_path, test_path, root, label='label')
+
+    model = CHyVAE(train, test, n_clusters=20, z_dim=args.z_dim, im_h=496, im_w=768, channels=1, batch_size=args.batch_size, n_epochs = args.n_epoch, nu=args.nu, prior_cov=np.eye(args.z_dim), run_no=args.run)
+
+    model.train()
---
+++ code/models_retinal.py
@@ -0,0 +1,174 @@
+from __future__ import print_function
+from __future__ import division
+import numpy as np
+import tensorflow as tf
+import time
+import os
+import datasets
+import utils
+import pickle
+from tqdm import tqdm
+ds = tf.contrib.distributions
+
+
+class CHyVAE:
+    def __init__(self, train_set, test_set, n_clusters, z_dim, im_h, im_w, channels, batch_size, n_epochs, nu, prior_cov, run_no=None):
+        self.trainset = train_set
+        self.testset = test_set
+        self.n_clusters = n_clusters
+        self.z_dim = z_dim
+        self.im_h = im_h
+        self.im_w = im_w
+        self.channels = channels
+        self.batch_size = batch_size
+        self.n_epochs = n_epochs
+        self.nu_np = nu
+        self.n_fc_units = 128 if dataset in {'2dshapes', 'correlated_ellipses'} else 256
+        scale = max(self.nu_np - self.z_dim - 1, 1)
+        self.Psi_np = scale * prior_cov
+        self.results_path = '../results/{:s}_z_{:d}_nu_{:d}_run{:d}/'.format(dataset, z_dim, nu, run_no)
+        if not os.path.exists(self.results_path):
+            os.makedirs(self.results_path)
+        self._build_model()
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
+        init_op = tf.global_variables_initializer()
+        self.sess.run(init_op)
+
+    def _encoder(self, x, reuse=False):
+        with tf.variable_scope('encoder', reuse=reuse):
+            z = tf.layers.conv2d(x, 32, 4, 2, padding='same', activation=tf.nn.relu)
+            z = tf.layers.conv2d(z, 32, 4, 2, padding='same', activation=tf.nn.relu)
+            z = tf.layers.conv2d(z, 64, 4, 2, padding='same', activation=tf.nn.relu)
+            z = tf.layers.conv2d(z, 64, 4, 2, padding='same', activation=tf.nn.relu)
+            z = tf.layers.flatten(z)
+            z = tf.layers.dense(z, self.n_fc_units, activation=tf.nn.relu)
+            mu = tf.layers.dense(z, self.z_dim, activation=None)
+            A = tf.reshape(tf.layers.dense(z, self.z_dim * self.z_dim, activation=None), [-1, self.z_dim, self.z_dim])
+            L = tf.matrix_band_part(A, -1, 0)
+            diag = tf.nn.softplus(tf.matrix_diag_part(A)) + 1e-4
+            L = tf.matrix_set_diag(L, diag)
+            L_LT = tf.matmul(L, L, transpose_b=True)
+            Sigma = L_LT + 1e-4 * tf.eye(self.z_dim)
+            return mu, Sigma
+
+    def _decoder(self, z, reuse=False):
+        with tf.variable_scope('decoder', reuse=reuse):
+            z = tf.layers.dense(z, self.n_fc_units, activation=tf.nn.relu)
+            z = tf.layers.dense(z, 4 * 4 * 64, activation=tf.nn.relu)
+            z = tf.reshape(z, [-1, 4, 4, 64])
+            z = tf.layers.conv2d_transpose(z, 64, 4, 2, padding='same', activation=tf.nn.relu)
+            z = tf.layers.conv2d_transpose(z, 32, 4, 2, padding='same', activation=tf.nn.relu)
+            z = tf.layers.conv2d_transpose(z, 32, 4, 2, padding='same', activation=tf.nn.relu)
+            x_logits = tf.layers.conv2d_transpose(z, self.channels, 4, 2, padding='same', activation=None)
+            return x_logits
+
+    def _regularizer(self, z, mu, Sigma, Psi, nu, B):
+        psi_zzT = Psi + tf.matmul(z, z, transpose_b=True)
+        mu = tf.expand_dims(mu, -1)
+        sigma_mumuT_psi = Sigma + tf.matmul(mu, mu, transpose_b=True) + Psi
+        return -\
+            0.5 * (nu + 1) * (tf.linalg.logdet(psi_zzT)) +\
+            0.5 * tf.linalg.logdet(Sigma) -\
+            0.5 * (nu + B) * tf.trace(tf.matmul(sigma_mumuT_psi, tf.matrix_inverse(psi_zzT)))
+
+    def _build_model(self):
+        # Model
+        self.x = tf.placeholder(tf.float32, [None, self.im_h, self.im_w, self.channels])
+        self.Psi = tf.placeholder(tf.float32, [self.z_dim, self.z_dim])
+        self.nu = tf.placeholder(tf.float32, ())
+        self.mu, Sigma = self._encoder(self.x)
+        mvn = ds.MultivariateNormalFullCovariance(loc=self.mu, covariance_matrix=Sigma)
+        z = mvn.sample()
+        z2 = tf.transpose(mvn.sample(1), perm=[1, 2, 0])
+        x_hat_logits = self._decoder(z)
+        self.loglikelihood = tf.reduce_mean(tf.reduce_sum(
+            tf.nn.sigmoid_cross_entropy_with_logits(labels=self.x, logits=x_hat_logits), [1, 2, 3]))
+        self.regularizer = -tf.reduce_mean(self._regularizer(
+            z2, self.mu, Sigma, self.Psi, self.nu, 1))
+        self.loss = self.loglikelihood + self.regularizer
+        self.optim_op = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
+
+        # Reconstruction
+        self.x_test = tf.placeholder(tf.float32, [None, self.imsize, self.imsize, self.channels])
+        z_test = self._encoder(self.x_test, reuse=True)[0]
+        self.x_recons = tf.nn.sigmoid(self._decoder(z_test, reuse=True))
+
+        # Generation
+        self.noise = tf.placeholder(tf.float32, [None, self.z_dim])
+        self.fake_images = tf.nn.sigmoid(self._decoder(self.noise, reuse=True))
+
+    def train(self):
+        train_path = os.path.join(self.results_path, 'train')
+        if not os.path.exists(train_path):
+            os.mkdir(train_path)
+        metrics_history = {'epoch': [], 'recons': [], 'disent': [], 'acc': []}
+        n_batch = len(self.trainset)
+        for epoch in range(1, self.n_epochs + 1):
+            print(f'epoch {epoch}')
+            loss = 0
+            loglh = 0
+            regr = 0
+            start_time = time.time()
+            for idx, (x_np, _) in enumerate(
+                    tqdm(self.trainset, desc='training', total=len(self.trainset), ascii=True)):
+                _, loss_np, rec_np, reg_np = self.sess.run([self.optim_op, self.loss, self.loglikelihood, self.regularizer], feed_dict={self.x: x_np, self.Psi: self.Psi_np, self.nu: self.nu_np})
+                loss += loss_np
+                loglh += rec_np
+                regr += reg_np
+
+            # epoch stats
+            end_time = time.time()
+            loss /= n_batch
+            loglh /= n_batch
+            regr /= n_batch
+            print('Epoch: {:d} in {:.2f}s:: Loss: {:.3f} => Recons.: {:.3f}, Reg: {:.3f}'.format(
+                epoch, end_time - start_time, loss, -loglh, -regr))
+
+            # epoch eval
+            for idx, (inputs, targets) in enumerate(
+                    tqdm(self.testset, desc='evaluation', total=len(self.testset), ascii=True)):
+                zi = self.sess.run(self.mu, feed_dict={self.x: inputs})
+                if idx == 0:
+                    mus = zi
+                    y = np.array(targets)
+                else:
+                    mus = np.concatenate((mus, np.array(zi)), axis=0)
+                    y = np.concatenate((y, np.array(targets)), axis=0)
+            disent_metric = utils.compute_metric_retinal(mus, y, self.z_dim, n_clusters = self.n_clusters)
+            metrics_history['epoch'].append(epoch)
+            metrics_history['recons'].append(-loglh)
+            metrics_history['disent'].append(disent_metric)
+            print('Metric: {:.4f}'.format(disent_metric))
+
+        with open(os.path.join(train_path, 'metrics.pkl'), 'wb') as pkl:
+            pickle.dump(metrics_history, pkl)
+
+    def generate(self):
+        gen_path = os.path.join(self.results_path, 'gen')
+        if not os.path.exists(gen_path):
+            os.mkdir(gen_path)
+        nu = self.z_dim + 1
+        while True:
+            z_np = utils.sample_noise(self.Psi_np, self.nu_np, 100)
+            x_hat_np = self.sess.run(self.fake_images, feed_dict={self.noise: z_np})
+            utils.render_images(x_hat_np, os.path.join(gen_path, 'nu_{:d}.png'.format(nu)))
+            if nu >= 2 * self.nu_np:
+                break
+            nu = nu * 4
+
+        x_test_np = next(self.test_batches)
+        x_np = np.vstack([x_test_np, next(self.train_batches), next(self.test_batches)])
+        means, = self.sess.run([self.mu], feed_dict={self.x: x_np})
+        for num, base_point in enumerate(means):
+            n_images_per_latent = 20
+            z_np = []
+            for i in range(self.z_dim):
+                dim_i = np.repeat(base_point[None, :], n_images_per_latent, 0)
+                dim_i[np.arange(n_images_per_latent), i] = np.linspace(-3, 3, n_images_per_latent)
+                z_np.append(dim_i)
+            z_np = np.vstack(z_np)
+            x_hat_np = self.sess.run(self.fake_images, feed_dict={self.noise: z_np})
+            img_path = os.path.join(gen_path, 'inter_{:d}_{:d}.png'.format(1, num))
+            utils.render_images(x_hat_np, img_path, n_rows=self.z_dim, n_cols=n_images_per_latent)
